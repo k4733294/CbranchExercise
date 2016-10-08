e210318e389d6faa9f1680af5e4b90e22047e1a8
MadeonL <k4733294@gmail.com>
2016-10-08 09:30:39 +0000

    Changes to be committed: 	new file:   ptrExer2.c

---
+++ ptrExer2.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ptrExer2.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/10/16 10:15:22
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Madeon LIU (ML), k4733294@gmail.comments
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	int *ptr;
+	int a;
+	//a = 2;
+	ptr = (int *)0x67a9;
+	printf("1231312444441424\n");
+	printf("prin ptr sizeof %d \n", sizeof(ptr));
+	printf("prin int sizeoc %d \n", sizeof(int));
+	printf("prin ptr %p \n", ptr);
+	//*ptr = 2;
+	//a = *ptr;
+	printf("prin *ptr to a %d", *ptr);
+	getchar();
+	return 0;
+
+}
+
